17d85a8f7786a46a73f9263b036040950736dba7
Mahad410 <mahadqureshi410@gmail.com>
2022-11-14 19:04:25 +0500

    Credit Card

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ const err_month = document.getElementById('err_exp');
 const err_num = document.getElementById('err_num');
 const err_name = document.getElementById('err_name');
 const err_cvv = document.getElementById('err_cvv');
+const thank_you = document.getElementById('contain');
+const form = document.getElementsByTagName('form')[0];
 // disable default behavior
 button.addEventListener('click', (e) => {
     e.preventDefault();
@@ -26,7 +28,7 @@ button.addEventListener('click', (e) => {
     let year_data = exp_year.value;
     let cvv_data = cvv.value;
     console.log({name_data, num_data, month_data, year_data, cvv_data});
-    if(name_data===''&& num_data==='' && month_data==='' && year_data==='' && cvv_data===''){
+    if (name_data === '' && num_data === '' && month_data === '' && year_data === '' && cvv_data === '') {
         name.style.border = '1px solid hsl(0, 100%, 66%)';
         c_num.style.border = '1px solid hsl(0, 100%, 66%)';
         exp_month.style.border = '1px solid hsl(0, 100%, 66%)';
@@ -36,28 +38,26 @@ button.addEventListener('click', (e) => {
         err_month.style.visibility = 'visible';
         err_num.style.visibility = 'visible';
         err_cvv.style.visibility = 'visible';
-    }
-    else if(name_data===''){
+    } else if (name_data === '') {
         name.style.border = '1px solid hsl(0, 100%, 66%)';
         err_name.style.visibility = 'visible';
-    }
-    else if(num_data===''){
+    } else if (num_data === '') {
         c_num.style.border = '1px solid hsl(0, 100%, 66%)';
         err_num.style.visibility = 'visible';
-    }
-    else if(month_data==='' || year_data===''){
+    } else if (month_data === '' || year_data === '') {
         exp_month.style.border = '1px solid hsl(0, 100%, 66%)';
         exp_year.style.border = '1px solid hsl(0, 100%, 66%)';
         err_month.style.visibility = 'visible';
-    }
-
-    else if( cvv_data===''|| cvv_data.length<4){
+    } else if (cvv_data === '' && cvv_data.length < 4) {
         cvv.style.border = '1px solid blue';
         err_cvv.style.visibility = 'visible';
         err_cvv.innerText = 'Invalid CVV';
     }
-
-    else{
+    else if(name_data !== '' && (num_data !== '' && num_data.length===19) && (month_data !== '' && month_data.length===2) && (year_data !== '' && year_data.length===2) && (cvv_data !== '' && cvv_data.length===3)){
+        form.style.display = 'none';
+        thank_you.style.display = 'flex';
+    }
+    else {
         name.style.border = '1px solid #ccc';
         c_num.style.border = '1px solid #ccc';
         exp_month.style.border = '1px solid #ccc';
@@ -67,6 +67,7 @@ button.addEventListener('click', (e) => {
         err_month.style.visibility = 'hidden';
         err_num.style.visibility = 'hidden';
         err_cvv.style.visibility = 'hidden';
+        thank_you.style.display='none';
     }
 })
 
@@ -75,10 +76,11 @@ button.addEventListener('click', (e) => {
 function onlyNumberKey(e) {
     // Only ASCII character in that range allowed
     let ASCIICode = (e.which) ? e.which : e.keyCode
-    if (ASCIICode === 9 || ASCIICode === 8 || (ASCIICode >= 48 && ASCIICode <= 57) || (ASCIICode >= 96 && ASCIICode <= 105))
+    if (ASCIICode===13 || ASCIICode === 9 || ASCIICode === 8 || (ASCIICode >= 48 && ASCIICode <= 57) || (ASCIICode >= 96 && ASCIICode <= 105))
         return true;
     return false;
 }
+
 c_num.onkeydown = onlyNumberKey;
 exp_month.onkeydown = onlyNumberKey;
 exp_year.onkeydown = onlyNumberKey;
@@ -108,11 +110,9 @@ c_num.oninput = function () {
         err_num.style.visibility = 'visible';
         c_num.style.border = '1px solid red';
         f_card_num.innerText = f_c_num;
-    }
-    else if(f_c_num.length === 0){
+    } else if (f_c_num.length === 0) {
         f_card_num.innerText = default_c_num;
-    }
-    else {
+    } else {
         f_card_num.innerText = f_c_num;
         err_num.style.visibility = 'hidden';
         c_num.style.border = '1px solid #ccc';
@@ -127,8 +127,7 @@ exp_month.oninput = function () {
         err_month.style.visibility = 'hidden';
         window.new_month = exp.innerText = exp_month.value;
         exp_month.style.border = '1px solid #ccc';
-    }
-    else {
+    } else {
         exp.innerText = default_exp;
         err_month.style.visibility = 'visible';
         err_month.innerText = 'Invalid Expire Date';
@@ -137,13 +136,12 @@ exp_month.oninput = function () {
 }
 // adding year
 exp_year.oninput = function () {
-    let year = new Date().getYear()-100;
-    if(exp_year.value < year || exp_year.value > year+5){
+    let year = new Date().getYear() - 100;
+    if (exp_year.value < year || exp_year.value > year + 5) {
         err_month.innerText = 'Invalid Year';
         err_month.style.visibility = 'visible';
         exp_year.style.border = '1px solid red';
-    }
-    else{
+    } else {
         err_month.style.visibility = 'hidden';
         exp_year.style.border = '1px solid #ccc';
         window.new_year = exp.innerText = new_month + ' / ' + exp_year.value;
@@ -154,11 +152,11 @@ exp_year.oninput = function () {
 cvv.oninput = function () {
     if (cvv.value.length > 0 && cvv.value.length < 4) {
         b_c_cvv.innerText = cvv.value;
-        cvv.style.border='1px solid #ccc';
+        cvv.style.border = '1px solid #ccc';
         err_cvv.style.visibility = 'hidden';
     } else {
         b_c_cvv.innerText = default_cvv;
-        cvv.style.border='1px solid red';
+        cvv.style.border = '1px solid red';
         err_cvv.style.visibility = 'visible';
     }
 }
